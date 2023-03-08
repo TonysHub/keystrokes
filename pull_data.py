ecf07d9099e7e446ea91819f20c84fd37051cd96
Tony <leegreen123@naver.com>
2023-03-08 15:50:04 +0900

    Changed the Naver API source code to Class object

--- pull_data.py
+++ pull_data.py
@@ -1,27 +1,54 @@
 #-*- coding: utf-8 -*-
 import os
 import sys
+import json
 import urllib.request
 from settings import *
 from datetime import date
 
-endDate = date.today().strftime("%Y-%m-%d")
-url = "https://openapi.naver.com/v1/datalab/search"
-body = "{\"startDate\":\"2020-01-01\",                                              \
-        \"endDate\":\endDate,                                                 \
-        \"timeUnit\":\"date\",                                                      \
-        \"keywordGroups\":[                                                         \
-            {\"groupName\":\"한글\",\"keywords\":[\"한글\",\"korean\"]},              \
-            {\"groupName\":\"영어\",\"keywords\":[\"영어\",\"english\"]}],}"
+import pandas as pd
+import numpy as np
 
-request = urllib.request.Request(url)
-request.add_header("X-Naver-Client-Id",CLIENT_ID)
-request.add_header("X-Naver-Client-Secret",CLIENT_SECRET)
-request.add_header("Content-Type","application/json")
-response = urllib.request.urlopen(request, data=body.encode("utf-8"))
-rescode = response.getcode()
-if(rescode==200):
-    response_body = response.read()
-    print(response_body.decode('utf-8'))
-else:
-    print("Error Code:" + rescode)
+pd.options.display.float_format = '{:.2f}'.format
+
+class NaverOpenAPI():
+    def __init__(self, client_id, client_secret):
+        # Import client details, intialize keywordGroups with None
+        self.client_id = CLIENT_ID
+        self.client_secret = CLIENT_SECRET
+        self.keywordGroups = []
+        self.url = "https://openapi.naver.com/v1/datalab/search"
+
+    def add_keyword_group(self, group_dict):
+        keyword_group = {
+            # Up to 5 groups, 20 keywords...
+            # Might have to call this function multiple times
+            'groupName': group_dict['groupName'],
+            'keywords': group_dict['keywords'] 
+        }
+        self.keywordGroups.append(keyword_group)
+    
+    def get_data(self, endDate, startDate, timeUnit, ages, gender, device):
+        # apply given data to params
+        params = json.dumps({
+            "startDate": startDate,
+            "endDate": endDate,
+            "timeUnit": timeUnit,
+            "keywordGroups": self.keywordGroups,
+            "device": device,
+            "ages": ages,
+            "gender": gender
+        }, ensure_ascii=False)
+        
+        # Below code is from NaverAPI 
+        request = urllib.request.Request(self.url)
+        request.add_header("X-Naver-Client-Id",self.client_id)
+        request.add_header("X-Naver-Client-Secret",self.client_secret)
+        request.add_header("Content-Type","application/json")
+        response = urllib.request.urlopen(request, data=params.encode("utf-8"))
+        rescode = response.getcode()
+        if(rescode==200):
+            result = json.loads(response.read())
+            print(result)
+        else:
+            print("Error Code:" + rescode)
